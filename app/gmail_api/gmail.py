4c06d4e2828dd8a42bb04655f30c76cdfc1f9f58
Hoang Huy Nguyen <huyhandsome189@gmail.com>
2024-08-18 22:19:46 -0400

    add comments to document the 'get_email_code' function

--- app/gmail_api/gmail.py
+++ app/gmail_api/gmail.py
@@ -1,10 +1,19 @@
 import re
 import os
+
 from simplegmail import Gmail
 from simplegmail.query import construct_query
 
+"""
+The Gmail API is a RESTful API that can be used to access Gmail mailboxes and send mail.
+
+This function connects to a Gmail account using credentials stored in a JSON file <client_secret.json>
+to retrieve/extract a 6-digit verification code from emails in a Gmail account.
+"""
 def get_email_code():
+    # Define the path to the Gmail API credentials file
     path = os.path.join(os.path.abspath("app\gmail_api\gmail_token.json"))
+
     gmail = Gmail(creds_file=path)
     
     # contructs a query
@@ -22,7 +31,10 @@ def get_email_code():
         print("Date: " + message.date)
         print("Preview: " + message.snippet)
         print("----------------------------")
+
+        # marked as read after retrieving the message
         message.mark_as_read()
+        
         if message.sender == "MEXC <dontreply@notification.mexc.link>" and message.subject == "[MEXC] Mã xác minh":
             # extract numbers from message.snippet
             nums_list = re.findall(r'\b\d+\b', message.snippet)
